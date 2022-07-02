689095dc4a9fe63c6efc899368ebcae0f135dd98
NumbersDeFi <arturo@tradeflows.io>
2022-07-02 22:13:48 +1000

    constants.ts cleanup

--- scripts/constants.ts
+++ scripts/constants.ts
@@ -1,41 +1,48 @@
 /** 15 min */
 export const TIMEOUT = 900_000;
-
-/** DEVNET **/
-export const FEE = BigInt('0'.replace(/,/g, ''))
 export const ETH_WEI = 10n ** 18n
 
-export const walletAddressOwner         = '0x5d1120755d9d5380201a8ac8bf39f7c4e2dd886a5b1431b7ea8dfb4ea3f0624'
-export const walletPrivateOwner         = '0xfa9ccf36421a514a2b8bcd75e06b884b'
+/** DEVNET **/
+export const FEE = BigInt('0'.replace(/,/g, '')) * ETH_WEI
 
-export const walletAddress0             = '0x7b85aa6f0bcb77bc76efafd22dfbb36055bd4915fd77d7d357fd7d3b366d9da'
-export const walletPrivate0             = '0x3f195a2cc7e18acfea7b737abbac88f2'
 
-export const walletAddress1             = '0x10b0a921fe94090bba9d65478edb9a2fe17857bf719574a58b39cbe22d92e7c'
-export const walletPrivate1             = '0xebd2156a6161176df1dc826d487b58c1'
+export const walletAddressOwner         = '.. GET YOUR OWN ..'
+export const walletPrivateOwner         = '.. GET YOUR OWN ..'
 
-export const daoContractAddress         = '0x03e122ff021fd9bf8952e55746ad095b751528ffcd5d40a4acc379a1d4431c25'
-export const erc20ContractAddress       = '0x02d704491ed20caa95afa45184b7e9c5ecf13e1a079fdbb5b95ea39ca592cf29'
-export const txFlowContractAddress      = '0x0746fffd49369c4bd3f5c970ff9885e0d4d3e6b6c43f2a15efea034dc61735a0'
-export const txDharmaContractAddress    = '0x030260d11b2cd88a410572b3011357857c9fcef9bd431d38c8dc0c47c7fdc835'
-export const txTradeContractAddress     = '0x05df41f469bf98be75e479385eee571ebedb72f70178fe00f17040f872c0b398'
+export const walletAddress0             = '.. GET YOUR OWN ..'
+export const walletPrivate0             = '.. GET YOUR OWN ..'
+
+export const walletAddress1             = '.. GET YOUR OWN ..'
+export const walletPrivate1             = '.. GET YOUR OWN ..'
+
+// Infrastructure
+export const daoContractAddress         = '.. GET YOUR OWN ..'
+export const txDharmaContractAddress    = '.. GET YOUR OWN ..'
+export const txTradeContractAddress     = '.. GET YOUR OWN ..'
+
+// ERC20
+export const erc20ContractAddress       = '.. GET YOUR OWN ..'
+export const txFlowContractAddress      = '.. GET YOUR OWN ..'
 
 /** GOERLI **/
-// export const FEE = BigInt('38866350207288'.replace(/,/g, ''))
-// export const ETH_WEI = 10n ** 18n
+// export const FEE = BigInt('10'.replace(/,/g, '')) * ETH_WEI
 
-// export const walletAddressOwner         = '0x5ea9751a81e959f854ad383dc97200e8c5e813a8bc3f8d6c51de8366b8fec46'
-// export const walletPrivateOwner         = '0xdb94347c36c1f3fddd54c1e5f7e56b5a'
+// export const walletAddressOwner         = '.. GET YOUR OWN ..'
+// export const walletPrivateOwner         = '.. GET YOUR OWN ..'
 
-// export const walletAddress0             = '0x01e2df9394250ec78ac85bae670d376301aec6c1d047df32962961777d6b14ba'
-// export const walletPrivate0             = '0x76745720e8b07e2f6835f69c63493dcbde3ae740e18908f940e31f159009701'
+// export const walletAddress0             = '.. GET YOUR OWN ..'
+// export const walletPrivate0             = '.. GET YOUR OWN ..'
 
-// export const walletAddress1             = '0x07a52439ce8c7f4bf0d271c2fb6a29e9d3c31054d0691ebe82986a9f9f241e76'
-// export const walletPrivate1             = '0x41cd6a6d7a83c66bd946adb9696d4bcc95558ced3eba680323d059dce7ca3b7'
+// export const walletAddress1             = '.. GET YOUR OWN ..'
+// export const walletPrivate1             = '.. GET YOUR OWN ..'
 
-// export const erc20ContractAddress       = '0x02e8a4b6fa457f60fb038d661690ed067c5c515022885ab34e6884a9524aad25'
-// export const txFlowContractAddress      = '0x01c2e7c375935aef0bdeb9ea80768dd3553ea58b025105640d40e1db597d984d'
-// export const txDharmaContractAddress    = '0x0722acf82b61f6f1ae4260571ea309e4177ead78186f66c3d0693692bb3377a0'
-// export const txTradeContractAddress     = '0x00ab723f826ab28fbdb65ba6aff45be5f92064d16e55bb35189df85a6b8fb931'
+// // Infrastructure
+// export const daoContractAddress         = '.. GET YOUR OWN ..'
+// export const txDharmaContractAddress    = '.. GET YOUR OWN ..'
+// export const txTradeContractAddress     = '.. GET YOUR OWN ..'
+
+// // ERC20
+// export const erc20ContractAddress       = '.. GET YOUR OWN ..'
+// export const txFlowContractAddress      = '.. GET YOUR OWN ..'
 
 
